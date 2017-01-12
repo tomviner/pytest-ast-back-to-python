85981559e22a4ba6d8c336c67b0140d75bd3dece
Tom Viner <tom@viner.tv>
2017-01-12 14:41:09 +0100

    bump version for Support and require pytest>=3.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-ast-back-to-python',
-    version='0.1.0',
+    version='1.0.0',
     author='Tom Viner',
     author_email='code@viner.tv',
     maintainer='Tom Viner',
